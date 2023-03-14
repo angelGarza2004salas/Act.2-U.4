599d6e407bca8c3255f8ef23d1b99d5b30d1e2a9
angelGarza2004salas <125837002+angelGarza2004salas@users.noreply.github.com>
2023-03-14 13:45:45 -0600

    Add files via upload

---
+++ Act.2 - U.4/Act.2 - U.4/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Act._2___U._4
+{
+    internal class Program
+    {
+         class Animal
+        {
+            public string Nombre { get; set; }
+            public int Edad { get; set; }
+        
+            public Animal(string nombre, int edad)
+            {
+                Nombre = nombre;
+                Edad = edad;
+            }
+
+            public void Comer()
+            {
+                Console.WriteLine("El animal está comiendo.");
+            }
+        }
+    
+        class Perro : Animal
+        {
+            public string Raza { get; set; }
+
+            public Perro(string nombre, int edad, string raza) : base(nombre, edad)
+            {
+                Raza = raza;
+            }
+
+            public void Ladrar()
+            {
+                Console.WriteLine("El perro está ladrando.");
+            }
+        }
+        
+        class Programa
+        {
+            static void Main(string[] args)
+            {
+                Perro miPerro = new Perro("Maverick", 6, "Labrador");
+
+                Console.WriteLine("Nombre del perro: " + miPerro.Nombre);
+                Console.WriteLine("Edad del perro: " + miPerro.Edad);
+                Console.WriteLine("Raza del perro: " + miPerro.Raza);
+
+                miPerro.Comer();
+                miPerro.Ladrar();
+            }
+        }
+
+    }
+}
